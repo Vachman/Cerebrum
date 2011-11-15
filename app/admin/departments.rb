80e5df7f94fc0b215c36a3e45fe56396285b9b29
Gevorkyan Vachagan <neyronsoft@gmail.com>
2011-11-15 10:40:18 +0400

    Employees м2

--- app/admin/departments.rb
+++ app/admin/departments.rb
@@ -1,4 +1,27 @@
 # encoding: utf-8
 ActiveAdmin.register Department do
   menu :label =>  "Отделы", :parent => "Сотрудники"
+
+  filter :name, :label => "Названию"
+
+  index do
+    column "Отдел", :name
+    default_actions
+  end
+  
+  show do
+    panel "Подробности" do
+      attributes_table_for department do
+        row("Отдел") { department.name }
+      end
+    end
+  end
+  
+  form do |f| 
+    f.inputs do
+      f.input :name, :label => "Отдел"
+    end
+    f.buttons
+  end
+
 end

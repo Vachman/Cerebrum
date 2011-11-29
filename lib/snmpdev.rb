d3fa155729b64805363b591675e4833251196861
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-11-29 17:17:55 +0400

    Deploy at 2011-11-29 17:17:55 +0400

--- lib/snmpdev.rb
+++ lib/snmpdev.rb
@@ -1,10 +1,15 @@
 require 'snmp'
 
 module SnmpDev                 
-  def get_value(oid)
-      SNMP::Manager.open(:Host => self.hostname, :timeout => 1, :retries => 1) do |manager|
-        manager.get_value(oid)   
-      end                        
+  def get_value(oid) 
+      return unless self.available?
+      begin
+        SNMP::Manager.open(:Host => self.hostname, :timeout => 1, :retries => 1) do |manager|
+          manager.get_value(oid)   
+        end
+      rescue Exception => e
+        puts e
+      end  
   end
 
   def sysDescr

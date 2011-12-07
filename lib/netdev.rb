daf4eac5f14ee6a5772f65d6ffc313f190adcbbd
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-08 00:24:30 +0400

    Deploy at 2011-12-08 00:24:30 +0400

--- lib/netdev.rb
+++ lib/netdev.rb
@@ -1,7 +1,7 @@
 module NetDev
   def available?
     `fping -a -t 50 "#{self.hostname}"`.empty? ? false : true  
-    logger.debug `fping -a -t 50 "#{self.hostname}"`
+    logger.debug "Hello world"
   end
   
   def update_lastms

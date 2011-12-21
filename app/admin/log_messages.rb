54952834458aebe668ba48dd4f1844a78d865329
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-21 17:55:22 +0400

    Deploy at 2011-12-21 17:55:22 +0400

--- app/admin/log_messages.rb
+++ app/admin/log_messages.rb
@@ -2,9 +2,19 @@
 ActiveAdmin.register LogMessage do
   menu :label => "Логи", :parent => "Оборудование"  
   
+  scope :all, :default => true
+  scope "Информация", :info
+  scope "Ошибки", :warn
+  
+  filter :hostname, :label => "IP"
+  filter :create_at, :label => "Дате"
+  
   index do
-    column :hostname
-    column :message
-    column :facility
+    column "Время" do |log_message|; log_message.log_time; end
+    column "Устройство" do |log_message|; log_message.hostname; end
+    column "Сообщение" do |log_message|; log_message.message; end
+    column "Стстус" do |log_message|
+      status_tag log_message.facility, (  log_message.facility.to_s == 'WARN' ? :error : :warning  )
+    end
   end
 end
--- app/models/log_message.rb
+++ app/models/log_message.rb
@@ -1,3 +1,6 @@
 class LogMessage < ActiveRecord::Base
   belongs_to :host
+  
+  scope :info, where('facility = ?', 'INFO')
+  scope :warn, where('facility = ?', 'WARN')
 end

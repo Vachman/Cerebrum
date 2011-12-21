90e981469ba615ed44d8dab105046343bf122317
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-22 01:30:45 +0400

    Deploy at 2011-12-22 01:30:45 +0400

--- app/admin/hosts.rb
+++ app/admin/hosts.rb
@@ -39,7 +39,7 @@ ActiveAdmin.register Host do
           host.device_type.nil? ? (div :class => "center" do  '' end ) : host.device_type.model 
       end
       column "Логи" do |host|
-        host.name
+        host.log_messages.count.to_s
       end 
       column "Состояние" do |host| 
         if host.lastms.is_a?(Time) 

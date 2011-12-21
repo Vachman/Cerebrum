b816a5ae366550669cbc18ca704e530949290f0c
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-21 17:35:01 +0400

    Deploy at 2011-12-21 17:35:01 +0400

--- app/admin/log_messages.rb
+++ app/admin/log_messages.rb
@@ -1,4 +1,10 @@
 # encoding: utf-8
 ActiveAdmin.register LogMessage do
   menu :label => "Логи", :parent => "Оборудование"  
+  
+  index do
+    column :hostname
+    column :message
+    column :facility
+  end
 end

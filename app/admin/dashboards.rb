cdc7b057d979a3850cc1dedf971600faf5dca820
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-23 13:35:38 +0400

    Deploy at 2011-12-23 13:35:38 +0400

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 ActiveAdmin::Dashboards.build do
   
   # Define your dashboard sections here. Each block will be
@@ -7,29 +8,10 @@ ActiveAdmin::Dashboards.build do
   # == Simple Dashboard Section
   # Here is an example of a simple dashboard section
   #
-     section "Recent Posts" do
+     section "Recent Posts" do     
        table_for Host.order("log_count Desc").limit(10) do |t|
           t.column("IP Адрес") { |host| link_to host.name, admin_host_path(host)}
-          t.column "Устройство" do |host|
-            host.device_type.model unless host.device_type.nil? 
-          end
-          column "Состояние" do |host| 
-            if host.lastms.is_a?(Time) 
-              status_tag (host.lastms > Time.now-2.minute ? 'Доступен' : 'Недоступен' ), 
-              ( host.lastms > Time.now-2.minute ? :ok : :error ), 
-              :title => time_ago_in_words(host.lastms)  
-            else
-              status_tag "Неизвестно", :title => "Сроду не видели"
-            end
-          end
-          t.column("Подъедз") { |host|  host.porch }
-          t.column("Местонахождение") do |host| 
-            status_tag host.location, ( host.location.eql?('Чердак') ? :ok : :warning ) unless host.location.nil? or host.location.empty?
        end
-         
-  #       Post.recent(5).collect do |post|
-  #         li link_to(post.title, admin_post_path(post))
-  #       end
      end
   
   # == Render Partial Section

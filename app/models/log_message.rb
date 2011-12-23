a29a19927b346fa420c41bfc87c0c9632f825da6
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2011-12-23 12:36:34 +0400

    Deploy at 2011-12-23 12:36:34 +0400

--- app/models/log_message.rb
+++ app/models/log_message.rb
@@ -4,12 +4,15 @@ class LogMessage < ActiveRecord::Base
   scope :info_log, where('facility = ?', 'INFO')
   scope :warn_log, where('facility = ?', 'WARN')
   
+  
+  
   #Logger
   Thread.new do
     File.open('/var/log/switches.log').each_line do |line|
       log_time = Time.parse(line.split[0..2].join(' '))
       hostname = line.split[3]
       host = Host.find_by_hostname(hostname)
+    
       # Without time
       if line.split[4].split('.').count.eql?(4) 
         facility = line.split[5].chop
@@ -20,7 +23,7 @@ class LogMessage < ActiveRecord::Base
         message = line.split[7..-1].join(' ')
       #  puts "-- LogTime: #{log_time}, Hostname: #{hostname}, Facility: #{facility}, Message: #{message}, Host: #{host.id}"
       end
-
+      host.update_attribute('log_count', h.log_messages.count += 1) if !hots.nil?
       LogMessage.create(:host => host, :log_time => log_time, :facility => facility, :message => message, :hostname => hostname)
     end
   end

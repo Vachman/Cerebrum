fdb1210a4cc6aa4c30c3e5ca9cfd869cf6cf8416
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2012-01-19 15:05:02 +0400

    Deploy at 2012-01-19 15:05:02 +0400

--- lib/devices/Dgs_3200_10g.rb
+++ lib/devices/Dgs_3200_10g.rb
@@ -7,7 +7,11 @@ PORST_COUNT = 10
   def ports_count
     PORST_COUNT
   end
-
+  
+  def snmp_location
+    get_snmp_value '1.3.6.1.2.1.1.6.0'
+  end
+  
   def testfunc
     puts 'Des 3200-10g Test'
   end

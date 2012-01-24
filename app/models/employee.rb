021778233ef232bbaf3fadf6c598ee6eb0800168
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2012-01-24 15:16:24 +0400

    Deploy at 2012-01-24 15:16:24 +0400

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,7 @@ class Employee < ActiveRecord::Base
   validates_presence_of :name, :message => "can't be blank"
   validates_presence_of :position, :message => "can't be blank"
   validates_associated :department
-  validates_format_of :PHONE, :with => /^\d{3}-\d{3}-\d{2}-\d{2}$/, :message => "Valid format: 916-123-45-67", :allow_blank => true
+  validates_format_of :number, :with => /^\d{3}-\d{3}-\d{2}-\d{2}$/, :message => "Valid format: 916-123-45-67", :allow_blank => true
 
   def page_title 
     self.name

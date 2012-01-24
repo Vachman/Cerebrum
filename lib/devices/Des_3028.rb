c313606d845b5d1dfa6a86f393e620433823feb8
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2012-01-24 15:04:44 +0400

    Deploy at 2012-01-24 15:04:44 +0400

--- lib/devices/Des_3028.rb
+++ lib/devices/Des_3028.rb
@@ -106,7 +106,6 @@ module DLinkDes3028FastEthernetSwitch
   end
   
   def port_status(port)
- #   puts "Port_status IF-MIB::ifOperStatus.#{port}"
     if port.is_numeric?
       status = get_snmp_value "IF-MIB::ifOperStatus.#{port}"
     #  puts "Status is #{status}"

bde47300b7d82b538be46456fa80a1a2022f18a1
Gevorkyan Vachcgan <neyronsoft@gmail.com>
2012-01-31 15:06:53 +0400

    Deploy at 2012-01-31 15:06:53 +0400

--- db/migrate/20120131100358_change_order_services_sevice_id_to_service_id.rb
+++ db/migrate/20120131100358_change_order_services_sevice_id_to_service_id.rb
@@ -1,6 +1,6 @@
 class ChangeOrderServicesSeviceIdToServiceId < ActiveRecord::Migration
   def up
-    rename_column :order_Services, :sevice_id, :service_id
+    rename_column :order_services, :sevice_id, :service_id
   end
 
   def down
